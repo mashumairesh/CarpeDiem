7e6ec786017be99b27418fb1e2234e83e76e5178
cimkoreauniv <changilmoon@korea.ac.kr>
2023-01-07 15:23:14 +0900

    Remove cardobject

--- Assets/Scripts/CardObject.cs
+++ Assets/Scripts/CardObject.cs
@@ -7,47 +7,6 @@ using Unity.VisualScripting;
 
 public class CardObject : MonoBehaviour
 {
-
-    [SerializeField] private Image _image;
-    [SerializeField] private CardData _cardData;
-    
-    public CardObject (int cardNum, List<int> price, List<int> effect, int turn, int slot)
-    {
-        _cardData.CardNum = cardNum;
-        _cardData.Price = price;
-        _cardData.Effect = effect;
-        _cardData.Turn = turn;
-        _cardData.Slot = slot;
-        _cardData.isControlAble = true;
-    }
-    public int GetCardNum()
-    {
-        return _cardData.CardNum;
-    }
-    public List<int> GetPrice()
-    {
-        return _cardData.Price;
-    }
-    public List<int> GetEffect()
-    {
-        return _cardData.Effect;
-    }
-    public int GetTurn()
-    {
-        return _cardData.Turn;
-    }
-    public int GetSlot()
-    {
-        return _cardData.Slot;
-    }
-    public bool GetCA()
-    {
-        return _cardData.isControlAble;
-    }
-    public void Puchased()
-    {
-        _cardData.isControlAble = false;
-    }
 }
 
 [System.Serializable]
@@ -58,5 +17,4 @@ public class CardData
     public List<int> Effect; //[7]
     public int Turn;
     public int Slot;
-    public bool isControlAble;
 }
