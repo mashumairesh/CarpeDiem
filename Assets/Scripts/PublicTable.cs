438bc9e005963e2ffb3714d3d519923c95157027
nick11967 <nick11967@snu.ac.kr>
2023-01-07 17:56:08 +0900

    create ImageManger

--- Assets/Scripts/PublicTable.cs
+++ Assets/Scripts/PublicTable.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class PublicTable : MonoBehaviour
 {
     private List<GameObject> _cards;
-    private Deck _deck;
 
     void AddformDeck()
     {
-        CardScript newcard = new CardScript;
-        newcard.Initalize(_deck.Draw());
-        _cards.Add();
+        
     }
 }
