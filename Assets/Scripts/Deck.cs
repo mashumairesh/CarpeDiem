d5138826de93e48edb79e89c02269506fd214675
cimkoreauniv <changilmoon@korea.ac.kr>
2023-01-07 15:26:47 +0900

    enable deck list access

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
-    private List<CardData> cards;
+    private List<CardData> _cards;
     public bool doShuffle;
     // Start is called before the first frame update
     void Awake()
@@ -19,24 +19,26 @@ public class Deck : MonoBehaviour
     void LoadCards()
     {
         StreamReader fs = new StreamReader(Path.Combine(Application.dataPath, "Resources/Json/cards.json"));
-        this.cards = JsonUtility.FromJson<CardLoadData>(fs.ReadToEnd()).cards;
+        this._cards = JsonUtility.FromJson<CardLoadData>(fs.ReadToEnd()).cards;
     }
 
     void Shuffle()
     {
-        for(int i=0; i<cards.Count - 1; i++) {
-            int rnd = Random.Range(i, cards.Count - 1);
-            CardData tmp = cards[i];
-            cards[i] = cards[rnd];
-            cards[rnd] = tmp;
+        for(int i=0; i<_cards.Count - 1; i++) {
+            int rnd = Random.Range(i, _cards.Count - 1);
+            CardData tmp = _cards[i];
+            _cards[i] = _cards[rnd];
+            _cards[rnd] = tmp;
         }
     }
     public CardData Draw()
     {
-        CardData ret = cards[cards.Count - 1];
-        cards.RemoveAt(cards.Count - 1);
+        CardData ret = _cards[_cards.Count - 1];
+        _cards.RemoveAt(_cards.Count - 1);
         return ret;
     }
+
+    public List<CardData> cards { get { return _cards; } }
 }
 
 public class CardLoadData
